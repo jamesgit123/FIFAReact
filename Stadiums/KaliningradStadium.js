f5d5b7cb32f7b44969870782f9903c4a3a879a5e
James Line <durianlone@hotmail.co.uk>
2018-04-02 13:21:29 +1000

    Add files via upload

---
+++ Stadiums/KaliningradStadium.js
@@ -0,0 +1,88 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, ScrollView, Linking } from 'react-native';
+import {StackNavigator} from 'react-navigation';
+import navStyle from './navStyles';
+import { MapView, MapViewDirections } from 'expo';
+
+export default class KaliningradStadium extends Component {
+
+  render() {
+    return (
+      <ScrollView>
+      <View style={styles.container}>
+
+      <View style = {styles.image}>
+      <Image
+      style={{width: 420, height: 100, }}
+      source={require('./KaliningradStadium.png')}
+       />
+        </View>
+
+      <MapView
+      style={{ flex: 1, height: 500, width: 300, padding: 50}}
+      region={{
+        latitude: 54.7108487,
+        longitude: 20.4769812,
+        latitudeDelta: 0.0100,
+        longitudeDelta: 0.0100,
+      }}
+    >
+
+    <MapView.Marker
+      coordinate = {{
+        latitude: 56.8399,
+        longitude: 60.6057
+      }}
+        title = {'San Francisco'}
+        description = {'My description'}
+    />
+    </MapView>
+
+    <Text style = {styles.titleText}> Here is a picture link </Text>
+    
+          <TouchableOpacity onPress = {() => Linking.openURL('http://www.google.com')}>
+            <Image
+            style={{width: 420, height: 400, }}
+            source={require('./KaliningradStadium.png')}
+            />
+          </TouchableOpacity>
+      
+      </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  titleText: {
+    fontSize: 28,
+    color: 'yellow',
+    textAlign: 'center',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    margin: 15,
+    height: 50,
+    width: 50,
+  },
+  buttonText: {
+    fontSize: 18,
+  },
+  image: {
+    paddingTop: 10,
+  },
+});
---
+++ Stadiums/LuzhnikiStadium.js
@@ -0,0 +1,90 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, ScrollView, Linking } from 'react-native';
+import {StackNavigator} from 'react-navigation';
+import navStyle from './navStyles';
+import { MapView, MapViewDirections } from 'expo';
+
+export default class LuzhnikiStadium extends Component {
+
+  render() {
+    return (
+      <ScrollView>
+      <View style={styles.container}>
+
+      <View style = {styles.image}>
+      <Image
+      style={{width: 420, height: 100, }}
+      source={require('./LuzhnikiStadium.png')}
+       />
+        </View>
+
+      <Text style = {styles.titleText}> You Are Here! </Text>
+      <MapView
+      style={{ flex: 1, height: 500, width: 300, padding: 50}}
+      region={{
+        latitude: 55.7168334,
+        longitude: 37.5550618,
+        latitudeDelta: 0.0100,
+        longitudeDelta: 0.0100,
+      }}
+    >
+
+    <MapView.Marker
+      coordinate = {{
+        latitude: 55.7168334,
+        longitude: 37.5550618
+      }}
+        title = {'San Francisco'}
+        description = {'My description'}
+    />
+    </MapView>
+      
+    <Text style = {styles.titleText}> Here is a picture link </Text>
+    
+          <TouchableOpacity onPress = {() => Linking.openURL('http://www.google.com')}>
+            <Image
+            style={{width: 420, height: 400, }}
+            source={require('./LuzhnikiStadium.png')}
+            />
+          </TouchableOpacity>
+     
+
+      </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  titleText: {
+    fontSize: 28,
+    color: 'yellow',
+    textAlign: 'center',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    margin: 15,
+    height: 50,
+    width: 50,
+  },
+  buttonText: {
+    fontSize: 18,
+  },
+  image: {
+    paddingTop: 10,
+  },
+});
